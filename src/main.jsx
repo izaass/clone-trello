dbdb5468d0f9c0e87b29129777c81b70b8908f41
Hazel <mr.izaass@gmail.com>
2023-11-27 10:47:29 +0700

    Update source code

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import theme from "./theme";
+
+import ReactDOM from "react-dom/client";
+
 import CssBaseline from "@mui/material/CssBaseline";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>
 );
---
+++ src/theme.js
@@ -0,0 +1,26 @@
+import { teal, orange, cyan, deepOrange } from "@mui/material/colors";
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const theme = extendTheme({
+  soTay: {
+    appBarHeight: "48px",
+    boardHeight: "59px",
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange,
+      },
+    },
+  },
+});
+
+export default theme;
